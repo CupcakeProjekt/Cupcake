ca724dde8e7bdf79bcc6615192b3942f2321aff4
Mikkelmattern <130869851+Mikkelmattern@users.noreply.github.com>
2026-04-10 10:47:19 +0200

    Finished login and createUser functions

--- src/main/java/app/controllers/UserController.java
+++ src/main/java/app/controllers/UserController.java
@@ -14,6 +14,20 @@ public class UserController {
 
     public static void addRouts(Javalin app, ConnectionPool connectionPool) {
         app.post("/login", ctx -> login(ctx, connectionPool));
+        app.post("/create-user", ctx -> createUser(ctx, connectionPool));
+
+    }
+
+    private static void createUser(Context ctx, ConnectionPool connectionPool) throws DatabaseException {
+        String username = ctx.formParam("username");
+        String password = ctx.formParam("password");
+        try{
+            User user = UserMapper.createUser(username, password, connectionPool);
+            ctx.sessionAttribute("currentUser", user);
+            ctx.redirect("/main-page");
+        } catch (DatabaseException e) {
+            throw new DatabaseException("Problem ved oprettelse af bruger", e.getMessage());
+        }
     }
 
     private static void login(Context ctx, ConnectionPool connectionPool) {
@@ -23,9 +37,14 @@ public class UserController {
         try {
             User user = UserMapper.login(username, password, connectionPool);
             ctx.sessionAttribute("currentUser", user);
+            ctx.redirect("/");
         } catch (DatabaseException e) {
             ctx.attribute("msg", e.getMessage());
             ctx.redirect("/");
         }
+    }
+    private static void logout(Context ctx){
+        ctx.req().getSession().invalidate();
+        ctx.redirect("/");
     }
 }
