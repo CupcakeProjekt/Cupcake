b68fa5d8efae46f43cac96d4a886e31dfe19f2b9
Mikkelmattern <130869851+Mikkelmattern@users.noreply.github.com>
2026-04-11 16:22:33 +0200

    Added Top and Bottom mapper

--- src/main/java/app/controllers/UserController.java
+++ src/main/java/app/controllers/UserController.java
@@ -1,8 +1,12 @@
 package app.controllers;
 
+import app.entities.Bottom;
+import app.entities.Topping;
 import app.entities.User;
 import app.exceptions.DatabaseException;
+import app.persistence.BottomMapper;
 import app.persistence.ConnectionPool;
+import app.persistence.TopMapper;
 import app.persistence.UserMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
@@ -15,13 +19,19 @@ public class UserController {
     public static void addRouts(Javalin app, ConnectionPool connectionPool) {
         app.post("/login", ctx -> login(ctx, connectionPool));
         app.post("/create-user", ctx -> createUser(ctx, connectionPool));
+        app.get("/index", ctx -> {
+            List<Topping> tops = TopMapper.getAllTops(connectionPool);
+            List<Bottom> bottoms = BottomMapper.getAllBottoms(connectionPool);
+            ctx.attribute("tops", tops);
+            ctx.attribute("bottoms", bottoms);
+        });
 
     }
 
     private static void createUser(Context ctx, ConnectionPool connectionPool) throws DatabaseException {
         String username = ctx.formParam("username");
         String password = ctx.formParam("password");
-        try{
+        try {
             User user = UserMapper.createUser(username, password, connectionPool);
             ctx.sessionAttribute("currentUser", user);
             ctx.redirect("/main-page");
@@ -43,7 +53,8 @@ public class UserController {
             ctx.redirect("/");
         }
     }
-    private static void logout(Context ctx){
+
+    private static void logout(Context ctx) {
         ctx.req().getSession().invalidate();
         ctx.redirect("/");
     }
