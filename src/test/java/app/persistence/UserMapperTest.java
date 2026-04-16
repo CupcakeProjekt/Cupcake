3d707e144625d0c414799d1a56340b1ae23568b6
Nali1234-lab <naliahmadi00@gmail.com>
2026-04-16 02:40:05 +0200

    usermapper skal arver fra basetest til at få daterne. denne test er færdig.

--- src/test/java/app/persistence/UserMapperTest.java
+++ src/test/java/app/persistence/UserMapperTest.java
@@ -1,24 +1,53 @@
 package app.persistence;
 
+import app.entities.User;
+import app.exceptions.DatabaseException;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class UserMapperTest {
+class UserMapperTest extends BaseTest {
+
 
     @Test
-    void login() {
+    void login() throws DatabaseException {
+        // Act
+        User user = UserMapper.login("test@test.dk", "1234", connectionPool);
+
+        // Assert - tjek at brugeren findes
+        assertNotNull(user);
+        assertEquals("test@test.dk", user.getEmail());
+
     }
 
     @Test
-    void createUser() {
+    void createUser() throws DatabaseException {
+        // Arrange
+        String uniqueEmail = "ny_" + System.currentTimeMillis() + "@test.dk";
+
+        // Act
+        User user = UserMapper.createUser(uniqueEmail, "1234", connectionPool);
+
+        // Assert
+        assertNotNull(user);
+        assertEquals(uniqueEmail, user.getEmail());
     }
 
     @Test
-    void getAllUsers() {
+    void getAllUsers() throws DatabaseException {
+        List<User> users = UserMapper.getAllUsers(connectionPool);
+
+        assertNotNull(users);
+        assertFalse(users.isEmpty());
     }
 
     @Test
-    void getUserByID() {
+    void getUserByID() throws DatabaseException {
+        User user = UserMapper.getUserByID(connectionPool, 1);
+
+        assertNotNull(user);
+        assertEquals(1, user.getUserID());
     }
 }
