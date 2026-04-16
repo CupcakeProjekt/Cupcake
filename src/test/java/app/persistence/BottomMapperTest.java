d6ecc9f52243d64c19b045237decbdb90ce14339
Nali1234-lab <naliahmadi00@gmail.com>
2026-04-16 02:07:27 +0200

    BottomMapperTest er færdig. ind i databasen skal jeg lave en beforeAll.

--- src/test/java/app/persistence/BottomMapperTest.java
+++ src/test/java/app/persistence/BottomMapperTest.java
@@ -1,7 +1,11 @@
 package app.persistence;
 
+import app.entities.Bottom;
+import app.exceptions.DatabaseException;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,15 +13,22 @@ class BottomMapperTest extends BaseTest {
 
 
     @BeforeAll
+    // BaseTest klarer allerede reset af database. så behøve man ikke denne metode.
     static void setup() {
 
     }
 
     @Test
-    void getAllBottoms() {
+    void getAllBottoms() throws DatabaseException {
+        List<Bottom> bottoms = BottomMapper.getAllBottoms(connectionPool);
+        assertNotNull(bottoms);
     }
 
-    @Test
-    void getBottomByID() {
-    }
+
+        @Test
+        void getBottomByID() throws DatabaseException {
+            // Act & Assert
+            assertNotNull(BottomMapper.getBottomByID(connectionPool, 1));
+        }
+
 }
